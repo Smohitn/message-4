241a9eb1ef9a758660c679f4d4eca9ba603a26ee
Smohitn <144104705+Smohitn@users.noreply.github.com>
2023-11-25 23:03:30 +0530

    Update message.js

--- message.js
+++ message.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Yay, I love you princess. Lets be the cutest and hottest couple ever.";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
